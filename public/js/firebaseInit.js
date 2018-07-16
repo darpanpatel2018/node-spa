be5cf28d6ce80f6c99cd2128ef411d83637cddf6
dappspatel1211 <33789149+dappspatel1211@users.noreply.github.com>
2018-07-16 21:49:49 +0530

    first commit

---
+++ public/js/firebaseInit.js
@@ -0,0 +1,10 @@
+// Initialize Firebase(Firebase Sample Project)
+var config = {
+  apiKey: "AIzaSyAfZf7o8XuCGEiMDJ4tMdG7wiPIdsp-JK4",
+    authDomain: "ambrai-5f376.firebaseapp.com",
+    databaseURL: "https://ambrai-5f376.firebaseio.com",
+    projectId: "ambrai-5f376",
+    storageBucket: "ambrai-5f376.appspot.com",
+    messagingSenderId: "493344501341"
+  };
+  var init = firebase.initializeApp(config);
